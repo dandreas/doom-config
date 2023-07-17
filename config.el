cd10e57b2b7dada5cdeae0c48080ef65c9f4e3d9
Dillon Andreas <belfieldcecil@gmail.com>
2023-07-17 17:00:18 -0500

    Merge more ini from my scratch emacs

--- config.el
+++ config.el
@@ -32,15 +32,68 @@
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-(setq doom-theme 'doom-monokai-pro)
+(setq doom-theme 'doom-monokai-classic
+      doom-font (font-spec :family "Hack" :size 12 :weight 'medium))
+
+;; Kill buffer command
+(global-set-key (kbd "C-x k") 'kill-current-buffer)
+;; Indent region
+(global-set-key (kbd "C-c i") 'indent-region)
+(+global-word-wrap-mode)
+(setq projectile-project-search-path '("C:/Users/dillona/Documents/projects"
+                                       "C:/Users/dillona/Documents/msys2640/home/dillona/mapupdate"
+                                       ("C:/Git" . 1)))
+;; Eshell config
+(global-set-key (kbd "C-x e") #'eshell)
+(add-hook 'eshell-mode-hook (lambda ()
+    ; Quick switch to org
+    (eshell/alias "so"
+        (if (or (eq system-type 'ms-dos) (eq system-type 'windows-nt))
+            "cd C:/org/org-notes"
+            "cd ~/Documents/GitHub/org-notes"))
+    ; Opening files
+    (eshell/alias "e" "find-file $1")
+    (eshell/alias "ff" "find-file $1")
+    (eshell/alias "emacs" "find-file $1")
+    (eshell/alias "ee" "find-file-other-window $1")
+    ; Open in explorer
+    (eshell/alias "ex" "explorer .")
+    ; Git
+    (eshell/alias "gd" "magit-diff-unstaged")
+    (eshell/alias "gds" "magit-diff-staged")
+    ; Dired
+    (eshell/alias "d" "dired .")
+    ; The 'ls' executable requires the Gnu version on the Mac
+    (let ((ls (if (file-exists-p "/usr/local/bin/gls")
+                  "/usr/local/bin/gls"
+                "ls")))
+      (eshell/alias "ll" (concat ls " -AlohG --color=always")))))
+(add-hook 'eshell-mode-hook (lambda ()
+    (eshell/alias "iscc" "C:/Program Files (x86)/Inno Setup 6/ISCC /Q /O-")
+    (eshell/alias "sp" "cd C:/Users/dillona/Documents/projects")
+    (eshell/alias "spp" "cd C:/Users/dillona/Documents/projects/0tools/python")
+    (eshell/alias "sl" "cd C:/Git/leos")
+    (eshell/alias "sli" "cd C:/Git/dct_installer")
+    (eshell/alias "slt" "cd C:/Git/location_tool")
+    (eshell/alias "slti" "cd C:/Git/map_updater")
+    (eshell/alias "si" "cd C:/Git/web_framework")
+    (eshell/alias "ss" "cd C:/Git/sara")
+    (eshell/alias "st" "cd C:/Git/ibars_report_templates")
+    (eshell/alias "sm" "cd C:/Users/dillona/mapupdate")))
+
+(set-register ?g (find-file (or "C:/Users/dillona/agsdb.ini" "")))
+(set-register ?h (find-file (or "C:/Users/dillona/leos_agsdb.ini" "")))
+(set-register ?i (find-file (or "C:/Users/dillona/lt_config.xml" "")))
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
-(setq display-line-numbers-type t)
+(setq display-line-numbers-type 'relative)
 
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
-(setq org-directory "~/org/")
+(if (or (eq system-type 'ms-dos) (eq system-type 'windows-nt))
+    (setq org-directory '"C:/org/org-notes")
+    (setq org-directory '"~/Documents/GitHub/org-notes"))
 
 
 ;; Whenever you reconfigure a package, make sure to wrap your config in an
