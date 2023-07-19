7c296bf89b2483d264143f4c1b7c979c208c8b17
Dillon Andreas <belfieldcecil@gmail.com>
2023-07-18 23:49:38 -0500

    Org configuration, match more to vanilla ini

--- config.el
+++ config.el
@@ -40,9 +40,12 @@
 ;; Indent region
 (global-set-key (kbd "C-c i") 'indent-region)
 (+global-word-wrap-mode)
-(setq projectile-project-search-path '("C:/Users/dillona/Documents/projects"
-                                       "C:/Users/dillona/Documents/msys2640/home/dillona/mapupdate"
-                                       ("C:/Git" . 1)))
+(if (or (eq system-type 'ms-dos) (eq system-type 'windows-nt))
+    (setq projectile-project-search-path '("C:/Users/dillona/Documents/projects"
+                                           "C:/Users/dillona/Documents/msys2640/home/dillona/mapupdate"
+                                           ("C:/Git" . 1)))
+    (setq projectile-project-search-path '(("~/Documents/GitHub" . 1))))
+
 ;; Eshell config
 (global-set-key (kbd "C-x e") #'eshell)
 (add-hook 'eshell-mode-hook (lambda ()
@@ -81,9 +84,9 @@
     (eshell/alias "st" "cd C:/Git/ibars_report_templates")
     (eshell/alias "sm" "cd C:/Users/dillona/mapupdate")))
 
-(set-register ?g (find-file (or "C:/Users/dillona/agsdb.ini" "")))
-(set-register ?h (find-file (or "C:/Users/dillona/leos_agsdb.ini" "")))
-(set-register ?i (find-file (or "C:/Users/dillona/lt_config.xml" "")))
+(if (or (eq system-type 'ms-dos) (eq system-type 'windows-nt)) (set-register ?g (find-file (or "C:/Users/dillona/agsdb.ini" ""))))
+(if (or (eq system-type 'ms-dos) (eq system-type 'windows-nt)) (set-register ?h (find-file (or "C:/Users/dillona/leos_agsdb.ini" ""))))
+(if (or (eq system-type 'ms-dos) (eq system-type 'windows-nt)) (set-register ?i (find-file (or "C:/Users/dillona/lt_config.xml" ""))))
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
@@ -95,6 +98,69 @@
     (setq org-directory '"C:/org/org-notes")
     (setq org-directory '"~/Documents/GitHub/org-notes"))
 
+;; Org keybinds
+(map! :after org
+      :map org-mode-map
+      "C-c ." 'org-time-stamp
+      "C-c x" 'org-cut-subtree)
+(global-set-key (kbd "C-c l") #'org-store-link)
+(global-set-key (kbd "C-c a") #'org-agenda)
+(global-set-key (kbd "C-c c") #'org-capture)
+
+;; Org capture templates
+(after! org
+        (setq org-enforce-todo-dependencies t
+              org-tag-persistent-alist '((:startgroup . nil)
+                                         ("@dct" . ?d)
+                                         ("@lt" . ?l)
+                                         ("@ibars" . ?i)
+                                         ("@sara" . ?s)
+                                         ("@pivots" . ?p)
+                                         ("@other" . ?o)
+                                         ("@home" . ?h)
+                                         (:endgroup . nil)
+                                         ("cuervo-burrito" . ?c)
+                                         ("side-project" . ?r)
+                                         ("emacs" . ?e)
+                                         ("tracs" . ?t)
+                                         ("nd")
+                                         ("sd")
+                                         ("ks")
+                                         ("nj")
+                                         (:startgroup . nil)
+                                         ("p1" . ?1)
+                                         ("p2" . ?2)
+                                         ("p3" . ?3)
+                                         ("p4" . ?4)
+                                         ("p5" . ?5)
+                                         (:endgroup . nil))
+              org-enfocre-todo-checkbox-dependencies t
+              org-todo-keywords '((sequence "TODO" "IN-PROGRESS" "WAITING" "|" "DONE" "CLOSED"))
+              org-todo-state-tags-triggers '(("CLOSED" ("ARCHIVE" . t)))
+              org-agenda-include-diary t
+              org-log-done 'time
+              diary-file (concat org-directory "/diary")
+              org-journal-dir (concat org-directory "/journal")
+              org-journal-file-format "%Y-%m.org"
+              org-journal-file-type 'monthly
+              ;; Set default directories, files
+              org-default-notes-file (concat org-directory "/inbox.org")
+              org-work-file (concat org-directory "/work.org")
+              org-home-file (concat org-directory "/home.org")
+              org-capture-templates '(("t" "Inbox" entry (file+headline org-default-notes-file "Inbox")
+                                       "* TODO %?\nDEADLINE: \n:PROPERTIES:\n:CREATION: %U\n:END:\n")
+                                      ("i" "Work Issue" entry (file+headline org-work-file "Inbox")
+                                       "* TODO %?\nDEADLINE: \n:PROPERTIES:\n:CREATION: %U\n:TRACKZILLA: N/A\n:END:\n")
+                                      ("h" "Home Task" entry (file+headline org-home-file "Tasks")
+                                       "* TODO %?\nDEADLINE: \n:PROPERTIES:\n:CREATION: %U\n:END:\n")
+                                      ("j" "Journal" entry (file+datetree org-default-notes-file)
+                                       "* %?\n%U\n%a")
+                                      ("n" "Link Inbox" entry (file+headline org-default-notes-file "Inbox")
+                                       "* TODO %?\nDEADLINE: \n:PROPERTIES:\n:CREATION: %U\n:END:\n%a")
+                                      ("o" "Link Work Issue" entry (file+headline org-work-file "Inbox")
+                                       "* TODO %?\nDEADLINE: \n:PROPERTIES:\n:CREATION: %U\n:END:\n%a")
+                                      ("m" "Link Home Task" entry (file+headline org-home-file "Tasks")
+                                       "* TODO %?\nDEADLINE: \n:PROPERTIES:\n:CREATION: %U\n:END:\n%a"))))
 
 ;; Whenever you reconfigure a package, make sure to wrap your config in an
 ;; `after!' block, otherwise Doom's defaults may override your settings. E.g.
